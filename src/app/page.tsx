0c09bfa1b3e8b5d6c04d9770bd1d40a0259c9ef6
Nishkarsh Kumar <nishkarsh.kumar@invenics.com>
2024-02-05 07:52:36 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,73 @@
+
+'use client'
+import React from "react";
+import signUp from "@/firebase/auth/signUp"
+import { useRouter } from 'next/navigation'
+import { sign } from "crypto";
+import signIn from "@/firebase/auth/signIn";
+
+function Home() {
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const router = useRouter()
+
+  const handleForm = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const { result, error } = await signIn(email, password);
+
+    if (error) {
+      return console.log(error)
+    }
+
+    // else successful
+    console.log(result)
+    return router.push("/admin")
+  }
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4 bg-white">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+          </div>
+
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" onSubmit={handleForm} method="POST">
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                <div className="mt-2">
+                  <input id="email" name="email" type="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                  <div className="text-sm">
+                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <input id="password" name="password" type="password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+              </div>
+
+              <div>
+                <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+              </div>
+            </form>
+
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Not a member?
+              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </main>
+  );
+}
+
+export default Home;
